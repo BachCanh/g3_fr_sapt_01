100a2539232a159c01828915522c321acc2d11e0
minhnghia-coder <dangnghia03052003@gmail.com>
2026-07-23 21:47:47 +0700

    fix index

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log('App started');
 function add(a, b) { return a + b; }
 console.log("Phien ban cua CanhBD6");
 console.log("Phien ban cua BaoNNG3");
+console.log("Phien ban cua NGHIADM3");
